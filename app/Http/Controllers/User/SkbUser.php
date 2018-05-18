bf0ea4063f6b82aeb8822494be45ebc8bc2928b0
xiaoji <552121229l@gmail.com>
2018-05-18 15:52:07 +0800

    自动注册用户

--- app/Http/Controllers/User/SkbUser.php
+++ app/Http/Controllers/User/SkbUser.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Traits\Tool;
+use App\Models\User\SkbUsersModel as SkbUsers;
 use QCloud_WeApp_SDK\Conf as Config;
 use QCloud_WeApp_SDK\Auth\LoginService as LoginService;
 use QCloud_WeApp_SDK\Constants as Constants;
@@ -20,13 +21,13 @@ class SkbUser extends Controller
 
         if ($result['loginState'] === Constants::S_AUTH) {
             return Tool::jsonResp([
-                'code' => 0,
-                'data' => $result['userinfo']
+                'err' => 0,
+                'dat' => $result['userinfo']
             ]);
         } else {
             return Tool::jsonResp([
-                'code' => -1,
-                'data' => []
+                'err' => -1,
+                'msg' => []
             ]);
         }
     }
@@ -35,15 +36,35 @@ class SkbUser extends Controller
         $result = LoginService::login();
 
         if ($result['loginState'] === Constants::S_AUTH) {
+
+            $users  = new SkbUsers();
+
+            $user   = $users->where('openid','=',$result['userinfo']['userinfo']->openId)->first();
+
+            if($user){
+                return Tool::jsonResp([
+                    'err' => 0,
+                    'dat' => $user
+                ]);
+            }
+
+            //注册用户 //by jizw
+            $user = ['username'  =>  '',
+                'openid'    =>  $result['userinfo']['userinfo']->openId,
+                'nickname'  =>  $result['userinfo']['userinfo']->nickName,
+                'avatar'    =>  $result['userinfo']['userinfo']->avatarUrl,
+                'created_at'=>  date('Y-m-d H:i:s'),
+                'updated_at'=>  date('Y-m-d H:i:s')];
+            $users->insert($user);
+
             return Tool::jsonResp([
-                'code' => 0,
-                'data' => $result['userinfo']
-            ]);
-        } else {
-            return Tool::jsonResp([
-                'code' => -1,
-                'error' => $result['error']
+                'err' => 0,
+                'dat' => $user
             ]);
         }
+            return Tool::jsonResp([
+                'err' => -1,
+                'msg' => $result['error']
+            ]);
     }
 }
