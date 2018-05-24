2885344ee6a1cf0254651118a3511b7c8f85372e
SoulMate94 <code0809@163.com>
2018-05-24 10:46:46 +0800

    add address

--- app/Models/Common/SkbAddress.php
+++ app/Models/Common/SkbAddress.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class SkbAddress extends Model
 {
     protected $table = 'skb_address';
+
+    public function delOnceAddress($id)
+    {
+        return $this->where('id', $id)->delete();
+    }
 }
