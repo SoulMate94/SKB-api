811f71502e9354d681d8d05246520e2cf9fd641a
SoulMate94 <code0809@163.com>
2018-05-24 17:40:23 +0800

    debug regex

--- app/Http/Controllers/Master/SkbBankCard.php
+++ app/Http/Controllers/Master/SkbBankCard.php
@@ -52,7 +52,7 @@ class SkbBankCard extends Controller
             'real_name'   => 'required|string',
             'ID_number'   => [
                 'required',
-                'regex:/(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}[0-9Xx]$)/',
+                'regex:/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}(\d|x|X)$/',
             ],
             'bank_reserve_mobile' => [
                 'required',
