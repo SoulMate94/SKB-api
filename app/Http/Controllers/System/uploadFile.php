08d57f56f49a97df9163039954ac5d4e907171e8
xiaoji <552121229l@gmail.com>
2018-07-20 14:49:39 +0800

    测试文件debug

--- app/Http/Controllers/System/uploadFile.php
+++ app/Http/Controllers/System/uploadFile.php
@@ -18,7 +18,7 @@ class uploadFile extends Controller
     {
         $folder   = $req->get('folder');
         $identity = $req->get('identity') == 1 ? 'master':'user';
-        return $identity;
+        return $req->get('identity');
 
         if(!$req->hasFile('skbPublicFile')) {
             return Tool::jsonR(-1,'File error', null);
