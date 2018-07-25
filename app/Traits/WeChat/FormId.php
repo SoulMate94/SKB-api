e982b307f53f456b2d8ec68307989a7c13b2cdae
xiaoji <552121229l@gmail.com>
2018-07-25 14:55:26 +0800

    测试form_id存储

--- app/Traits/WeChat/FormId.php
+++ app/Traits/WeChat/FormId.php
@@ -50,11 +50,12 @@ class FormId
                             ->first();
 
         if($formId){
-            $formIds->update('is_use', 1)->where([
-                ['open_id', '=', $open_id],
-                ['form_id', '=', $formId],
-                ['is_use', '=', '0']
-            ]);
+            $formIds->where([
+                    ['open_id', '=', $open_id],
+                    ['form_id', '=', $formId],
+                    ['is_use', '=', '0']
+                ])
+                ->update('is_use', 1);
             return $formId->form_id;
         }
 
