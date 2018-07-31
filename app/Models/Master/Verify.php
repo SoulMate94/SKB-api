eae804e9938221dec971c2793271170c57178edf
xiaoji <552121229l@gmail.com>
2018-07-31 16:30:38 +0800

    master verify debug

--- app/Models/Master/Verify.php
+++ app/Models/Master/Verify.php
@@ -11,6 +11,10 @@ class Verify extends Model
     protected $primaryKey = 'id';
     public $timestamps    = false;
 
+    protected $casts = [
+        'id_card_img'       => 'json'
+    ];
+
     //插入master verify数据
     public function insertVerify($params)
     {
@@ -50,7 +54,7 @@ class Verify extends Model
 
         $params['work_area']        = json_encode($params['work_area']);
         $params['product_type_id']  = json_encode($params['product_type_id']);
-        $params['id_card_img']      = json_encode($params['id_card_img']);
+        $params['id_card_img']      = $params['id_card_img'];
         $params['service_sta_time'] = strtotime($params['service_sta_time']);
         $params['service_end_time'] = strtotime($params['service_end_time']);
 
