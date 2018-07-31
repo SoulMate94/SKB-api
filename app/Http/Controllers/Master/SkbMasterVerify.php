100a8f0fefc2c7fb57f27f01814cc2bec89e9e78
xiaoji <552121229l@gmail.com>
2018-07-31 13:31:14 +0800

    master verify debug

--- app/Http/Controllers/Master/SkbMasterVerify.php
+++ app/Http/Controllers/Master/SkbMasterVerify.php
@@ -197,6 +197,8 @@ class SkbMasterVerify extends Controller
         $params['mid']         = $ssn->get('user')['id'];
         $params['id_card_img'] = Tool::uploadFile($req,'id_card_img', 'masterVerify');
 
+        var_dump($params['mid']);die;
+
         $dat = $verify->insertVerify($params);
 
         if ($dat) {
